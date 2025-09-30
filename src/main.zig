f0e489b83567b347503166bd2e6664ad20a916f4
YonYon2 <yonathantorr@gmail.com>
2025-09-30 16:51:51 -0400

    Finished timer class

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,16 @@ const ray = @cImport({
 const Timer = struct {
     const Self = @This();
     start: f64,
-    life: f64,
+    life: f64, // duration; constant
     pub fn makeTimer() Timer {
         return .{ .start = 0, .life = 0 };
     }
     pub fn startTimer(self: *Self, life: f64) void {
         self.start = ray.GetTime();
         self.life = life;
+    }
+    pub fn resetTimer(self: *Self) void {
+        self.start = ray.GetTime();
     }
     pub fn timerDone(self: Self) bool {
         return ray.GetTime() - self.start >= self.life;
@@ -28,25 +31,31 @@ pub fn main() !void {
 
     ray.SetTargetFPS(60);
 
-    var timer = Timer.makeTimer();
-    var alarm: f64 = 0;
+    // init
+    var alarm: [2]Timer = .{ Timer.makeTimer(), Timer.makeTimer() }; // every 0.5s and 0.1s
+    alarm[0].startTimer(0.5);
+    alarm[1].startTimer(0.1);
     var buf = std.mem.zeroes([1024]u8);
-    var flash = true;
-    // extra
+    var flash: [2]bool = .{ true, true };
     while (!ray.WindowShouldClose()) {
-        timer.startTimer(0);
-        if (alarm >= 1000) {
-            flash = !flash;
-            alarm -= 1000;
+        // logic and calc
+        const dt = alarm[0].getElapsed();
+
+        for (&alarm, 0..) |*a, i| {
+            if (a.timerDone()) {
+                a.resetTimer();
+                flash[i] = true;
+            } else flash[i] = false;
         }
+        const flash_c1: u21 = if (flash[0]) '@' else ' ';
+        const flash_c2: u21 = if (flash[1]) '@' else ' ';
+        _ = try std.fmt.bufPrintZ(&buf, "{d:x<8.6}\n{d:x<8.6}\n[{u}]\n[{u}]", .{ 3.14, dt, flash_c1, flash_c2 });
+
+        // everything drawing
         ray.BeginDrawing();
         defer ray.EndDrawing();
 
         ray.ClearBackground(ray.RAYWHITE);
-        const flash_c: u8 = if (flash) '*' else ' ';
-        const frame_time = timer.getElapsed();
-        _ = try std.fmt.bufPrint(&buf, "{d}\n{d}\n[{c}]", .{ frame_time, alarm, flash_c });
         ray.DrawText(&buf, 190, 200, 20, ray.LIGHTGRAY);
-        alarm += frame_time;
     }
 }
