8f59faffb12db678db5f7171ded18e825ee225f1
YonYon6 <yonathantorr@gmail.com>
2026-04-09 17:02:09 -0400

    feat: pawn from-to squares are parsed

--- src/main.zig
+++ src/main.zig
@@ -236,6 +236,7 @@ const pgnReader = struct {
         var start_read = false;
         const Modes = enum { move_number, move, clock, timestamp };
         var mode = Modes.move_number;
+        var players = [_]Player{ .init(false), .init(true) };
         for (remaining_file, 0..) |c, i| {
             switch (mode) {
                 .move_number => {
@@ -261,6 +262,33 @@ const pgnReader = struct {
                                 'N' => if (is_white) Pieces.N else Pieces.n,
                                 else => if (is_white) Pieces.P else Pieces.p,
                             };
+                            // var from = Square.default;
+                            // find which pawn we moving
+                            if (move_piece == .P or move_piece == .p) {
+                                // d4, bxc5, a1=Q
+                                const attack = move_text[1] == 'x';
+                                const file_from = move_text[0]; // regardless if its an attack, the first character is the originating file
+                                // const file_to = if (attack) move_text[2] else ('a'-1);
+                                // const rank = if (attack) move_text[3] else move_text[1];
+                                for (players[@intFromBool(is_white)].pawn) |*p| {
+                                    if (p.alive and p.square.file == @as(u3, file_from - 'a')) {
+                                        const next_rank: u3 = @truncate(move_text[if (attack) 3 else 1] - '1');
+                                        const is_negative = p.square.rank > next_rank;
+                                        const dist = if (is_negative) p.square.rank - next_rank else next_rank - p.square.rank;
+                                        if (dist == 1) {
+                                            // this the one!
+                                            if (is_negative) {
+                                                p.square.rank -= 1;
+                                            } else {
+                                                p.square.rank += 1;
+                                            }
+                                            if (attack) {
+                                                p.square.file = move_text[2];
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                             try move_list.append(allocator, .{ .mover = move_piece });
                             std.debug.print("{s} ", .{move_text});
                             start_read = false;
